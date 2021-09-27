a8efb4fb24b7ac1b4e0e2ae3f2007e73e86a23c7
Mufaqar Islam <mufaqar@gmail.com>
2021-09-27 19:51:03 +0500

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
       <div className="bg-hero">
-        <div className="container relative flex flex-col items-center justify-between min-h-screen mx-auto lg:flex-row ">
+        <div className="container flex  items-center justify-between py-44  mx-auto flex-row static overflow-hidden">
           <div className="w-full mt-10 lg:w-1/2 lg:mr-0">
             <h2 className="p-2 text-4xl font-extrabold leading-tight text-gray-900 lg:text-5xl font-fira ">
               Impacting Industry
@@ -19,26 +19,25 @@ export default function Hero() {
               right solutions.
             </p>
             <div className="flex p-2 mt-6 lg:p-0">
-              <Link href="/contact" className="">
-                <a className="p-4 text-white uppercase rounded-full pl-7 pr-7 hover:text-black bg-gradient-to-l from-blue-300 to-blue-500">
+              <Link href="/contact">
+                <a className="p-4 text-white font-semibold uppercase rounded-full pl-7 pr-7 hover:text-black bg-gradient-to-l from-blue-300 to-blue-500">
                   Start A project
                 </a>
               </Link>
-              <Link href="/contact" className="">
+              <Link href="/contact">
                 <a className="p-4 ml-6 text-gray-800 uppercase border-2 border-gray-800 rounded-full pl-7 pr-7 hover:bg-gradient-to-l from-blue-300 to-blue-500">
                   View portfolio
                 </a>
               </Link>
             </div>
           </div>
-          <div className="w-full mt-8 lg:w-1/2 lg:mt-0">
-            <Image
-              src="/images/heroimg.png"
-              className="absolute right-0"
-              width="872"
-              height="572"
-            />
-          </div>
+          <img
+            alt="moto"
+            src="/images/heroimg.png"
+            className="absolute right-0"
+            width="765"
+            height="558"
+          />
         </div>
       </div>
     </>
