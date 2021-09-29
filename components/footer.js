384c5f74d66ca40275f736178f768fa85843e577
Mufaqar Islam <mufaqar@gmail.com>
2021-09-29 19:54:27 +0500

    Updated
    Footer

--- components/footer.js
+++ components/footer.js
@@ -1,153 +1,113 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <>
-      <div className="bg-blue-100">
-        <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-          <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
-            <div className="sm:col-span-2">
-              <Link href="/">
-                <a
-                  aria-label="Go home"
-                  title="SoftGens"
-                  className="inline-flex items-center"
-                >
-                  <svg
-                    className="w-8 text-deep-purple-accent-400"
-                    viewBox="0 0 24 24"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    stroke="currentColor"
-                    fill="none"
-                  >
-                    <rect x="3" y="1" width="7" height="12" />
-                    <rect x="3" y="17" width="7" height="6" />
-                    <rect x="14" y="1" width="7" height="6" />
-                    <rect x="14" y="11" width="7" height="12" />
-                  </svg>
-                  <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                    SoftGens
-                  </span>
-                </a>
-              </Link>
-              <div className="mt-6 lg:max-w-sm">
-                <p className="text-lg text-gray-700 font-fira">
-                  A one-stop shop for businesses big and small. From building,
-                  to designing, to analyzing and improving, we are always ready
-                  to make your company website smarter.
-                </p>
-              </div>
+      <footer className="py-24 bg-gradient-to-l from-blue-300 to-blue-500">
+        <div className="container mx-auto">
+          <div className="flex flex-col items-center justify-between lg:flex-row">
+            <div className="flex ">
+              <ul className="flex flex-wrap justify-center font-sans font-normal lg:justify-left">
+                <li className="mr-3 lg:mr-8 ">
+                  <Link href="/">
+                    <a className="text-lg text-white hover:text-black">HOME</a>
+                  </Link>
+                </li>
+                <li className="mr-3 lg:mr-8">
+                  <Link href="/services">
+                    <a className="text-lg text-white hover:text-black">
+                      SERVICES
+                    </a>
+                  </Link>
+                </li>
+                <li className="mr-3 lg:mr-8 ">
+                  <Link href="/portfolio">
+                    <a className="text-lg text-white hover:text-black">
+                      PORTFOLIO
+                    </a>
+                  </Link>
+                </li>
+                <li className="mr-3 lg:mr-8">
+                  <Link href="/company">
+                    <a className="text-lg text-white hover:text-black">
+                      ABOUT US
+                    </a>
+                  </Link>
+                </li>
+                <li className="mr-3 lg:mr-8">
+                  <Link href="/contact">
+                    <a className="text-lg text-white hover:text-black">
+                      CONTACT US
+                    </a>
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="space-y-2 text-sm">
-              <p className="text-base font-bold tracking-wide text-gray-900">
-                Contacts
-              </p>
-              <div className="flex">
-                <p className="mr-1 text-gray-800">Phone:</p>
-                <a
-                  href="tel:850-123-5021"
-                  aria-label="Our phone"
-                  title="Our phone"
-                  className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
-                >
-                  850-123-5021
+            <div className="flex mt-5 lg:mt-0">
+              <span className="flex p-2 w-[36px] h-[36px]  mr-5 bg-white rounded-full hover:animate-bounce">
+                <a href="https://www.youtube.com/" target="_new">
+                  <Image
+                    src="/images/linkedin.png"
+                    alt=""
+                    className="cursor-pointer "
+                    width="20"
+                    height="20"
+                  />
                 </a>
-              </div>
-              <div className="flex">
-                <p className="mr-1 text-gray-800">Email:</p>
-                <a
-                  href="mailto:info@softgens.com"
-                  aria-label="Our email"
-                  title="Our email"
-                  className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
-                >
-                  info@softgens.com
-                </a>
-              </div>
-              <div className="flex">
-                <p className="mr-1 text-gray-800">Address:</p>
-                <a
-                  href="https://www.google.com/maps"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Our address"
-                  title="Our address"
-                  className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
-                >
-                  Suit 134 B AHS Multan Road Lahore Pakistan
-                </a>
-              </div>
-            </div>
-            <div>
-              <span className="text-base font-bold tracking-wide text-gray-900">
-                Social
               </span>
-              <div className="flex items-center mt-1 space-x-3">
-                <a
-                  href="https://www.facebook.com/mufaqar"
-                  className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                    <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-                  </svg>
+              <span className="flex p-2 w-[36px] h-[36px]  mr-5 bg-white rounded-full hover:animate-bounce">
+                <a href="https://www.youtube.com/" target="_new">
+                  <Image
+                    src="/images/facebook.png"
+                    alt=""
+                    className="cursor-pointer"
+                    width="20"
+                    height="20"
+                  />
                 </a>
-                <a
-                  href="https://www.facebook.com/mufaqar"
-                  className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                    <circle cx="15" cy="15" r="4" />
-                    <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-                  </svg>
+              </span>
+              <span className="flex p-2 w-[36px] h-[36px]  mr-5 bg-white rounded-full hover:animate-bounce">
+                <a href="https://www.youtube.com/" target="_new">
+                  <Image
+                    src="/images/twitter.png"
+                    alt=""
+                    className="cursor-pointer "
+                    width="20"
+                    height="20"
+                  />
                 </a>
-                <a
-                  href="https://www.facebook.com/mufaqar"
-                  className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                >
-                  <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                    <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-                  </svg>
-                </a>
-              </div>
-              <p className="mt-4 text-lg text-gray-700 font-fira">
-                We Love To Hear About Your Project
-              </p>
+              </span>
             </div>
           </div>
-          <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-            <p className="text-sm text-gray-600">
-              © 2021 Web Design Pakistan | Website Designers in Pakistan –
-              SoftGens. All rights reserved.
-            </p>
-            <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-              <li>
-                <Link href="/">
-                  <a className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/compnay">
-                  <a className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    About
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <a className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    Services
-                  </a>
-                </Link>
-              </li>
-            </ul>
+          <div className="flex flex-col justify-between mt-10 lg:flex-row">
+            <div>
+              <span className="">
+                <h1 className="font-sans text-4xl font-bold tracking-wider text-center text-white lg:text-left ">
+                  SOFTGENS
+                </h1>
+                <p className="hidden mt-4 text-xl font-light text-center text-white lg:text-left lg:block">
+                  Copyright © 2012-2021 Softgens Company.
+                  <br /> All rights reserved.
+                </p>
+              </span>
+            </div>
+            <div className="mt-8 lg:mt-0">
+              <p className="text-xl font-normal text-center text-white lg:text-right">
+                Lorem ipsum dolor sit amet.
+                <br />
+                dolor sitLorem, ipsum.
+              </p>
+              <p className="mt-6 text-xl font-semibold text-center text-white lg:text-right">
+                info@softgens.com
+              </p>
+              <p className="text-xl font-normal text-center text-white lg:text-right ">
+                +123 4567 7890
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      </footer>
     </>
   );
 }
