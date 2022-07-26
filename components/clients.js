975ef0259dc7b9c3b27a849111fbe303c9c5a6d2
zunair <gillanix007@gmail.com>
2022-07-26 04:45:10 -0700

    fixinglanding

--- components/clients.js
+++ components/clients.js
@@ -47,32 +47,7 @@ export default function Clients() {
               See What We do
             </a>
           </Link>
-          <div className="grid justify-center grid-cols-2 mt-5 md:grid-cols-3 lg:justify-start">
-            <div className="flex flex-col items-center justify-center mt-6 mr-8 lg:mr-20">
-              <h3 className="text-3xl font-semibold text-main lg:text-5xl">
-                250+
-              </h3>
-              <span className="items-center mt-3 text-xs font-medium text-gray-500 uppercase whitespace-nowrap">
-                Client Satisfied
-              </span>
-            </div>
-            <div className="flex flex-col items-center justify-center mt-6 mr-8 lg:mr-20">
-              <h3 className="text-3xl font-semibold text-main lg:text-5xl">
-                11
-              </h3>
-              <span className="items-center mt-3 text-xs font-medium text-gray-500 uppercase whitespace-nowrap">
-                years of experiance
-              </span>
-            </div>
-            <div className="flex flex-col items-center justify-center mt-6 mr-8 lg:mr-20">
-              <h3 className="text-3xl font-semibold text-main lg:text-5xl">
-                600+
-              </h3>
-              <span className="items-center mt-3 text-xs font-medium text-gray-500 uppercase whitespace-nowrap">
-                Projects build
-              </span>
-            </div>
-          </div>
+          
         </motion.div>
         <motion.div animate={animationLogo} className="mt-8 lg:mt-0 grid items-center justify-end w-full grid-cols-4 gap-1.5 p-3 lg:p-10 gap-y-0 item-center grid-row-3 lg:w-1/2 ">
           <div>
