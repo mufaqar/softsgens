1a52d74d4061585eced6755aacda75b2eedddfd0
Mufaqar Islam <mufaqar@gmail.com>
2022-08-18 11:56:32 +0500

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,58 +1,58 @@
 import Head from 'next/head';
 import Footer from '../components/footer';
-import Header from '../components/header'
+import Header from '../components/header';
 import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-hook-inview'
+import { useInView } from 'react-hook-inview';
 import { useEffect } from 'react';
 
-
 export default function ContactMe() {
-
   const [ref, inView] = useInView();
-    const animationOpacity = useAnimation();
-    const animation = useAnimation();
-    const formCircle = useAnimation();
+  const animationOpacity = useAnimation();
+  const animation = useAnimation();
+  const formCircle = useAnimation();
 
-    useEffect(() => {
-        if(inView){
-            animation.start({
-              x: 0,
-              opacity: 1
-            })
-            animationOpacity.start({
-                opacity: 1,
-                scale: 1,
-                transition: {
-                    type: "spring", duration: 1, bounce: 0.3
-                }
-            })
-            formCircle.start({
-              opacity: 1,
-              scale: 1,
-              transition: {
-                  type: "spring", duration: 2, bounce: 0.3
-              }
-          })
-        }else{
-            animationOpacity.start({
-                opacity: 0,
-                scale: 0.8
-            })
-            animation.start({
-              x: "3vw",
-              opacity: 0
-            })
-            formCircle.start({
-              opacity: 0,
-              scale: 0.8
-          })
-        }
-    }, [inView]);
-
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        opacity: 1,
+      });
+      animationOpacity.start({
+        opacity: 1,
+        scale: 1,
+        transition: {
+          type: 'spring',
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+      formCircle.start({
+        opacity: 1,
+        scale: 1,
+        transition: {
+          type: 'spring',
+          duration: 2,
+          bounce: 0.3,
+        },
+      });
+    } else {
+      animationOpacity.start({
+        opacity: 0,
+        scale: 0.8,
+      });
+      animation.start({
+        x: '3vw',
+        opacity: 0,
+      });
+      formCircle.start({
+        opacity: 0,
+        scale: 0.8,
+      });
+    }
+  }, [inView]);
 
   return (
     <>
-
       <Head>
         <title>Contact Us | SoftGens </title>
         <meta name="description" content="Contact Us" />
@@ -67,65 +67,188 @@ export default function ContactMe() {
       />
 
       <div className="container mx-auto mt-48 mb-28">
-        <div ref={ref} className="flex flex-col justify-between text-black md:flex-row ">
-          <motion.div className="flex flex-col justify-between space-y-8" animate={animationOpacity}>
+        <div
+          ref={ref}
+          className="flex flex-col justify-between text-black md:flex-row "
+        >
+          <motion.div
+            className="flex flex-col justify-between space-y-8"
+            animate={animationOpacity}
+          >
             <div>
               <h1 className="text-4xl font-bold tracking-wide">Contact Us</h1>
-              <p className="pt-2 text-sm text-gray-400">For all your non-existant company needs Fake Corp are <br/>here to help, do not hesitate to contact us!</p>
+              <p className="pt-2 text-sm text-gray-400">
+                For all your non-existant company needs Fake Corp are <br />
+                here to help, do not hesitate to contact us!
+              </p>
             </div>
             <div className="flex flex-col space-y-6">
               <div className="inline-flex items-center space-x-2 w-60">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#a5b4fc" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                <span>+111 345 214 213</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#a5b4fc"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-phone"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+                <span>+92 302 6006 280</span>
               </div>
               <div className="inline-flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#a5b4fc" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-                <span>hello@fakecorp.com</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#a5b4fc"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-mail"
+                >
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
+                </svg>
+                <span>hello@softsgens.com</span>
               </div>
               <div className="inline-flex items-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#a5b4fc" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-                <span>111 Fake Boulevard, Fakesville, FYC</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#a5b4fc"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-map-pin"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>
+                <span>Suit 134-B Multan Road Lahore, Pakistan</span>
               </div>
             </div>
             <div className="flex space-x-4">
               <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-facebook"
+                >
+                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                </svg>
               </a>
               <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-instagram"
+                >
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                </svg>
               </a>
               <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-twitter"
+                >
+                  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                </svg>
               </a>
               <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-linkedin"
+                >
+                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                  <rect x="2" y="9" width="4" height="12"></rect>
+                  <circle cx="4" cy="4" r="2"></circle>
+                </svg>
               </a>
             </div>
           </motion.div>
           <div className="relative">
-            <motion.div className="absolute z-0 w-40 h-40 bg-indigo-400 rounded-full -right-28 -top-28" animate={animationOpacity}></motion.div>
-            <motion.div className="absolute z-0 w-40 h-40 bg-indigo-400 rounded-full -left-28 -bottom-16" animate={animationOpacity}></motion.div>
-            <motion.div className="relative z-10 p-8 text-gray-600 bg-white shadow-lg rounded-xl md:w-[560px]" animate={animation}>
-              <form className="flex flex-col space-y-4" action="" >
+            <motion.div
+              className="absolute z-0 w-40 h-40 bg-indigo-400 rounded-full -right-28 -top-28"
+              animate={animationOpacity}
+            ></motion.div>
+            <motion.div
+              className="absolute z-0 w-40 h-40 bg-indigo-400 rounded-full -left-28 -bottom-16"
+              animate={animationOpacity}
+            ></motion.div>
+            <motion.div
+              className="relative z-10 p-8 text-gray-600 bg-white shadow-lg rounded-xl md:w-[560px]"
+              animate={animation}
+            >
+              <form className="flex flex-col space-y-4" action="">
                 <div>
                   <label className="text-sm">Name</label>
-                  <input type="text"
+                  <input
+                    type="text"
                     placeholder="Name"
-                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300" />
+                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300"
+                  />
                 </div>
                 <div>
                   <label className="text-sm">Email</label>
-                  <input type="email"
+                  <input
+                    type="email"
                     placeholder="Email"
-                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300" />
+                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300"
+                  />
                 </div>
                 <div>
                   <label className="text-sm">Message</label>
-                  <textarea placeholder="Message"
+                  <textarea
+                    placeholder="Message"
                     rows="4"
-                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300"></textarea>
+                    className="w-full px-4 py-2 mt-2 rounded-md outline-none ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-300"
+                  ></textarea>
                 </div>
-                <button className="self-end inline-block px-6 py-4 text-sm font-bold text-white uppercase bg-indigo-600 rounded-lg">Send Message</button>
+                <button className="self-end inline-block px-6 py-4 text-sm font-bold text-white uppercase bg-indigo-600 rounded-lg">
+                  Send Message
+                </button>
               </form>
             </motion.div>
           </div>
