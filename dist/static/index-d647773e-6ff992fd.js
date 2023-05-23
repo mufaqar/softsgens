167892909401188d7310d99e9196fd3e92c15102
mufaqar <mufaqar@gmail.com>
2023-05-23 14:30:15 +0500

    Sanity

---
+++ dist/static/index-d647773e-6ff992fd.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-9e58f0f1-892bf522.js";import"./desk-257f76bb-48440d02.js";import"./index-473283cc.js";export{m as DocumentPaneProvider,a as default};
